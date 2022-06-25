a59b50f6bec64d86fdba42cf970f3c3927f49552
Red Dust <comanche.red.dust@gmail.com>
2022-06-25 16:39:19 +0100

    Create class Definition in definition.py file

--- definition.py
+++ definition.py
@@ -1,7 +1,16 @@
+import pandas as pd
+
+
 class Definition:
 
     def __init__(self, termo):
         self.termo = termo
 
     def get(self):
-        pass
+        df = pd.read_csv('data.csv')
+        return tuple(df.loc[df['word']==self.termo]['definition'])
+
+
+inst_definition = Definition('volleyball')
+
+print(inst_definition.get())
